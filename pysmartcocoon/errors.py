70c004d6c17891c8ec73f19fe59e9f2510194d95
Dave Pearce <davepearce@live.com>
2023-08-16 13:09:28 +0000

    Updated files for linting

--- pysmartcocoon/errors.py
+++ pysmartcocoon/errors.py
@@ -4,25 +4,17 @@
 class SmartCocoonError(Exception):
     """Define a base error."""
 
-    pass
-
 
 class UnauthorizedError(SmartCocoonError):
     """Define an error related to invalid requests."""
-
-    pass
 
 
 class RequestError(SmartCocoonError):
     """Define an error related to invalid requests."""
 
-    pass
-
 
 class TokenExpiredError(SmartCocoonError):
     """Define an error for expired access tokens that can't be refreshed."""
-
-    pass
 
 
 ERROR_CODES = {1: "The email has not been validated"}
--- pysmartcocoon/location.py
+++ pysmartcocoon/location.py
@@ -9,10 +9,40 @@ class Location:  # pylint: disable=too-many-instance-attributes
 
     def __init__(self, data: dict[str, Any]) -> None:
         """Initialize."""
-        self.id: int = data["id"]
-        self.name: str = data["name"]
-        self.street: str = data["location"]["street"]
-        self.city: str = data["location"]["city"]
-        self.state: str = data["location"]["state"]
-        self.country: str = data["location"]["country"]
-        self.postal_code: str = data["location"]["postal_code"]
+        self._identifier: int = data["id"]
+        self._name: str = data["name"]
+        self._street: str = data["location"]["street"]
+        self._city: str = data["location"]["city"]
+        self._state: str = data["location"]["state"]
+        self._country: str = data["location"]["country"]
+        self._postal_code: str = data["location"]["postal_code"]
+
+    @property
+    def identifier(self) -> str:
+        """Return location id"""
+        return self._identifier
+
+    @property
+    def name(self) -> str:
+        """Return location mame"""
+        return self._name
+
+    @property
+    def street(self) -> str:
+        """Return street"""
+        return self._street
+
+    @property
+    def city(self) -> str:
+        """Return city"""
+        return self._city
+
+    @property
+    def country(self) -> str:
+        """Return country"""
+        return self._country
+
+    @property
+    def postal_code(self) -> str:
+        """Return postal code"""
+        return self._postal_code
